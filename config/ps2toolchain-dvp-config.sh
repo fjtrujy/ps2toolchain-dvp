919be6f8fc9c2c1526fc92488507d49991ba4613
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2024-05-04 17:53:22 +0200

    point to 2.24

--- config/ps2toolchain-dvp-config.sh
+++ config/ps2toolchain-dvp-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PS2TOOLCHAIN_DVP_BINUTILS_REPO_URL="https://github.com/fjtrujy/binutils-gdb.git"
-PS2TOOLCHAIN_DVP_BINUTILS_DEFAULT_REPO_REF="dvp-v2.19.1-linux"
+PS2TOOLCHAIN_DVP_BINUTILS_DEFAULT_REPO_REF="dvp-v2.24"
 
 if test -f "$PS2DEV_CONFIG_OVERRIDE"; then
   source "$PS2DEV_CONFIG_OVERRIDE"
